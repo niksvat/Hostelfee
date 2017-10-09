daa98db9c9d9ce0a1e92d753c56b19ac0c778a48
niks <sharmanikhil610@gmail.com>
2017-10-09 12:52:09 +0530

    commit upto payment done...

---
+++ src/java/hostel/connectionprovider.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hostel;
+
+/**
+ *
+ * @author Nikhil
+ */
+import java.sql.*;
+
+public class connectionprovider {
+   
+    String connectionurl="jdbc:mysql://localhost:3306/hostelfee";//mpmusicx_mp3{change according to requirnment}
+    String username="root";//niks
+    String password="nikhil";//jy7e8yba2
+    
+    Connection con = null;
+    
+    public connectionprovider(){
+    
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        con=DriverManager.getConnection(connectionurl,username,password);
+        
+    
+    }catch(Exception e){
+    
+        e.printStackTrace();
+    }
+    }
+    public Connection getCon(){  
+        return con;  
+    
+    
+    }
+    
+    
+}
